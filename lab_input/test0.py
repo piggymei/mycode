e519a7d48f40c399107457d9142e7b8bf822540e
Jane <yumeij.2010@gmail.com>
2020-08-10 17:12:34 -0400

    complete lab 8

---
+++ lab_input/test0.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+user_input = input("May i have your name:")
+print("Hello,", user_input)
+print("Let me get lunch early!")
+
---
+++ lab_input/test1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+name_input = input("May i have your name: ").title()
+day_of_week = input("What day of the week it is: ").title()
+print(f"Hello, {name_input}! Happy {day_of_week}!")
