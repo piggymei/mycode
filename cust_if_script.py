43e90d5c68fe7ff99d3fc0ed78564babcba671fe
Jane <yumeij.2010@gmail.com>
2020-08-12 11:07:15 -0400

    update

--- cust_if_script.py
+++ cust_if_script.py
@@ -21,12 +21,12 @@ questionNum = 0
 
 while(questionNum < 4):
     
-    print(questions[questionNum])
+    print("\n\n","*",questions[questionNum],"*","\n\n")
     num = 1
     for x in answers[questionNum]:
         print(str(num) + ". " + x + "\n")
         num += 1
-    answerInput = input("which do you choose? (1,2,3)\n")
+    answerInput = input("which do you choose? (1,2,3)\n\n\n")
     while int(answerInput) < 1 or int(answerInput) >=4:
         answerInput = input("please choose among (1,2,3)\n")
    
@@ -37,13 +37,13 @@ while(questionNum < 4):
     else:
         score += 3
     questionNum += 1
-
+    print("_________________________________________________________\n")
 if (score <= 4):
     print("you belong to city!")
 elif (score > 4 and score < 16):
-    print("your belong to country!")
+    print("you belong to country!")
 else:
-    print("your belong to county and city!")
+    print("you belong to city!")
 
 
 
