beced3da86c021104031ec93377e26665e6afe5d
Jane <yumeij.2010@gmail.com>
2020-08-19 15:27:43 +0000

    add

---
+++ nasa01/downloadimage.py
@@ -0,0 +1,19 @@
+import requests
+import wget
+
+datepic = input("what date do you like for the image? (YYYY-MM-DD) ")
+resolution = input("would you like the image in High definition or standard definition?(High or Standard) ").lower()
+
+API_KEY = "8EWWTP0IkiMrdVGucP6AOEMJsviwqjbPzHZ3zqLr"
+url = "https://api.nasa.gov/planetary/apod?date=" + datepic +"&api_key=" + API_KEY
+
+r = requests.get(url)
+resp = r.json()
+#print(resp)
+dateofpicture = resp["date"] 
+titleofpicture = resp["title"]
+descriptionofpicture = ["explanation"]
+pic_url = resp["url"]
+
+wget.download(pic_url, 'dateofpicture.jpg')
+
